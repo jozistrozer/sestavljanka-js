a604412a2d0b2750117dfb7ac6f95395451d6f34
jozistrozer <jozistrozer13@gmail.com>
2021-02-05 17:37:24 +0100

    Dodane slike in delujoča zamenjava slik

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
+function Vleci(ev){
+    ev.dataTransfer.setData("src", ev.target.id);
+    console.log(ev.target);
+}
+
+function Odvrzi(ev){
+    ev.preventDefault();
+    ev.preventDefault();
+    var src = document.getElementById(ev.dataTransfer.getData("src"));
+    var srcParent = src.parentNode;
+    var tgt = ev.currentTarget.firstElementChild;
+
+    ev.currentTarget.replaceChild(src, tgt);
+    srcParent.appendChild(tgt);
+}
+
+function DovoliOdvrzi(ev){
+    ev.preventDefault();
+}
+
 function UstvariSliko(){
     // Nastavljanje spremenljivk
     var puzzleContainer = document.getElementById("idPuzzleContainer");
@@ -7,19 +27,26 @@ function UstvariSliko(){
     puzzleContainer.innerHTML = "";
     
     // Nastavljanje število okenčkov
-    document.getElementById("idPuzzleContainer").style.gridTemplateColumns = "repeat("+izbranaTezavnost+", 50px)";
+    document.getElementById("idPuzzleContainer").style.gridTemplateColumns = "repeat("+izbranaTezavnost+", 100px)";
     
     // Zanka, ki izriše koščke slik
     for (var i = 0; i < izbranaTezavnost*izbranaTezavnost; i++){
         var noviDiv = document.createElement("div");
         var slikaDiv = document.createElement("img");
 
+        // Nastavljanje atributov div
         noviDiv.setAttribute("id", "podDiv"+i);
         noviDiv.setAttribute("class", "koscekSlike");
+        noviDiv.setAttribute("ondrop", "Odvrzi(event)");
+        noviDiv.setAttribute("ondragover", "DovoliOdvrzi(event)");
         
+        // Nastavljanje atribute koščkov
+        slikaDiv.setAttribute("id", "img"+i);
         slikaDiv.setAttribute("src", "slike/slika1/"+izbranaTezavnost+"x"+izbranaTezavnost+"/"+(i+1) + ".jpg");
-        slikaDiv.setAttribute("width", "50px");
-        slikaDiv.setAttribute("height", "50px");
+        slikaDiv.setAttribute("width", "100px");
+        slikaDiv.setAttribute("height", "100px");
+        slikaDiv.setAttribute("draggable", "true");
+        slikaDiv.setAttribute("ondragstart", "Vleci(event)");
 
         noviDiv.appendChild(slikaDiv);
         puzzleContainer.appendChild(noviDiv);
