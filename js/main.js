9a400e21896bccd0e65d049238a86057121e13ef
jozistrozer <jozistrozer13@gmail.com>
2021-02-05 16:46:55 +0100

    Preimenovanje slik

--- js/main.js
+++ js/main.js
@@ -12,8 +12,16 @@ function UstvariSliko(){
     // Zanka, ki izriše koščke slik
     for (var i = 0; i < izbranaTezavnost*izbranaTezavnost; i++){
         var noviDiv = document.createElement("div");
+        var slikaDiv = document.createElement("img");
+
         noviDiv.setAttribute("id", "podDiv"+i);
         noviDiv.setAttribute("class", "koscekSlike");
+        
+        slikaDiv.setAttribute("src", "slike/slika1/"+izbranaTezavnost+"x"+izbranaTezavnost+"/"+(i+1) + ".jpg");
+        slikaDiv.setAttribute("width", "50px");
+        slikaDiv.setAttribute("height", "50px");
+
+        noviDiv.appendChild(slikaDiv);
         puzzleContainer.appendChild(noviDiv);
     }
     
