ffaf75140c88730b9a0055a50d98e89dea8fdd47
jozistrozer <jozistrozer13@gmail.com>
2021-02-12 20:47:55 +0100

    Zmešanje koščkov, preverjanje koščkov

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 // Se proži, kadar košček zagrabimo
 function Vleci(ev){
     ev.dataTransfer.setData("src", ev.target.id);
-    console.log(ev.target);
 }
 
 // Se proži, kadar košček spustimo
 function Odvrzi(ev){
-    ev.preventDefault();
     ev.preventDefault();
     var src = document.getElementById(ev.dataTransfer.getData("src"));
     var srcParent = src.parentNode;
@@ -14,10 +12,32 @@ function Odvrzi(ev){
 
     ev.currentTarget.replaceChild(src, tgt);
     srcParent.appendChild(tgt);
+
+    preveriKoscke();
 }
 
 function DovoliOdvrzi(ev){
     ev.preventDefault();
+}
+
+function preveriKoscke(){
+    console.log("preverjam");
+    var izbranaTezavnost = document.getElementById("idTezavnost").value;
+    var potIzbranaTez = izbranaTezavnost*izbranaTezavnost;
+    var koscki = [];
+    var urejen = [];
+    // zmešan
+    for (var i = 0; i < potIzbranaTez; i++){
+        koscki.push(document.getElementById("podDiv"+i).firstChild.id);
+        urejen.push("img"+i);
+    }
+
+    if (koscki.toString() == urejen.toString()){
+        console.log("ZMAGA ZMAGA ZMAGA ZMGA");
+        for (var k = 0; k < potIzbranaTez; k++){
+            document.getElementById("img"+k).setAttribute("draggable", "false");
+        }
+    }
 }
 
 function UstvariSliko(){
@@ -26,6 +46,10 @@ function UstvariSliko(){
     var izbranaTezavnost = document.getElementById("idTezavnost").value;
     var randSlika = Math.floor(Math.random() * 5 + 1);
     var temp = null;
+    var potIzbranaTez = izbranaTezavnost*izbranaTezavnost;
+    var izbraneStevilke = [];
+    var rand_num;
+
     // Brisanje slik
     puzzleContainer.innerHTML = "";
     
@@ -33,7 +57,7 @@ function UstvariSliko(){
     document.getElementById("idPuzzleContainer").style.gridTemplateColumns = "repeat("+izbranaTezavnost+", 100px)";
     
     // Zanka, ki izriše koščke slik
-    for (var i = 0; i < izbranaTezavnost*izbranaTezavnost; i++){
+    for (var i = 0; i < potIzbranaTez; i++){
         var noviDiv = document.createElement("div");
         var slikaDiv = document.createElement("img");
 
@@ -45,9 +69,15 @@ function UstvariSliko(){
         
         if (randSlika == 1) { temp = 1;} else {temp=0;}
 
+        rand_num = Math.floor(Math.random() * potIzbranaTez);
+        while (izbraneStevilke.includes(rand_num)){
+            rand_num = Math.floor(Math.random() * potIzbranaTez);
+        }
+        izbraneStevilke.push(rand_num);
         // Nastavljanje atribute koščkov
-        slikaDiv.setAttribute("id", "img"+i);
-        slikaDiv.setAttribute("src", "slike/slika"+randSlika+"/"+izbranaTezavnost+"x"+izbranaTezavnost+"/"+(i+temp) + ".jpg");
+        slikaDiv.setAttribute("id", "img"+rand_num);
+        slikaDiv.setAttribute("src", "slike/slika"+randSlika+"/"+izbranaTezavnost+"x"+izbranaTezavnost+"/"+(rand_num+temp) + ".jpg");
+        
         slikaDiv.setAttribute("width", "100px");
         slikaDiv.setAttribute("height", "100px");
         slikaDiv.setAttribute("draggable", "true");
@@ -56,5 +86,4 @@ function UstvariSliko(){
         noviDiv.appendChild(slikaDiv);
         puzzleContainer.appendChild(noviDiv);
     }
-    
 }
